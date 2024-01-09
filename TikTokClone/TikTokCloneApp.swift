f3b34b984e645acf8de4c5cd6f461acf16e2aa44
johannpires <johann.pires.stu@gmail.com>
2024-01-09 15:07:11 +0000

    Initial Commit

---
+++ TikTokClone/TikTokCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  TikTokCloneApp.swift
+//  TikTokClone
+//
+//  Created by Johann Pires on 09/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct TikTokCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
